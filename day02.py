3b5f3b921f3309c83ab912b2e9df5713aa01af3a
jimin <jiminseo0307@gmail.com>
2021-06-02 01:35:33 +0900

    0602
    rename & practice

--- day02.py
+++ day02.py
@@ -4,20 +4,20 @@ Create the missing functions, use default arguments.
 Sometimes you have to use 'return' and sometimes you dont.
 Start by creating the functions
 """
-def is_on_list(s, x):
-  if x in s: 
-    return True
-  else:
-    return False  
+def is_on_list(a, b):
+    if b in a:
+        return True
+    else:
+        return False
 
-def get_x(s, n):
-  return s[n]
+def get_x(a, b):
+    return a[b]
 
-def add_x(s, x):
-  s.append(x)
+def add_x(a, b):
+    a.append(b)
 
-def remove_x(s, x):
-  s.remove(x)
+def remove_x(a, b):
+    a.remove(b)
   
 # \/\/\/\/\/\/\  DO NOT TOUCH AREA  \/\/\/\/\/\/\ #
 
@@ -35,3 +35,15 @@ print(days)
 
 
 # /\/\/\/\/\/\/\ END DO NOT TOUCH AREA /\/\/\/\/\/\/\ #
+
+# days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+
+# if  'Wed' in days:
+#     print("Is Wed on 'days' list?", True)
+
+# print("The forth item in 'days' is: ", days[3])
+
+# days.append('Sat')
+# print(days)
+
+# print(days[1:])
